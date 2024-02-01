a6eaf117fdaded3746f0bfc8ff2ca158aa81ba28
Damian Jamroży <damianjamrozy99@gmail.com>
2024-02-01 12:37:10 +0100

    0.9

--- assets/js/default.js
+++ assets/js/default.js
@@ -54,3 +54,28 @@ window.onload = function() {
     css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
     document.body.appendChild(css);
 };
+
+
+// Contact form
+$(document).ready(function () {
+    $('.form-input').on('keyup, paste, cut, focusout', function () {
+        var $parent = $(this).parents('.form-line');
+        var input_value = $.trim($(this).val());
+        var required = $(this).is(':required');
+
+        if (input_value.length > 0) {
+            $parent.find('label').addClass('top');
+
+            $parent
+                .removeClass('error')
+                .addClass('success');
+        } else {
+            $parent.find('label').removeClass('top');
+            $parent.removeClass('success')
+
+            if (required) {
+                $parent.addClass('error');
+            }
+        }
+    });
+});
